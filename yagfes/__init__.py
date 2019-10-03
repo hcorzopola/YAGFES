6a6e094d4a8bea78ff6843f0a10163593bbad833
hcorzopola <55563675+hcorzopola@users.noreply.github.com>
2019-10-02 21:47:40 -0500

    Create __init__.py

---
+++ yagfes/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@author: hcorzopola
+
+############################GW MODELLING EXAMPLES##############################
+"""
+
+name="yagfes"
+
+from yagfes import aux_f as aux
+from yagfes import fdm_m as fdm
+from yagfes import fem_m as fem
