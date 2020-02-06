93b0b10fb92a37f1183fb7d9d7fddb8cece0b010
hcorzopola <hcorzopola@gmail.com>
2020-02-05 21:18:47 -0600

    YAGFES v.0.9.0

--- yagfes/aux_f.py
+++ yagfes/aux_f.py
@@ -3,6 +3,22 @@
 """
 @author: hcorzopola
 
+    YAGFES: Yet Another Groundwater Flow Equation Solver
+    Copyright (C) 2019  H.A. Corzo Pola
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
 ###########################AUXILIAR FUNCTIONS MODULE###########################
 
 This module includes all object classes and functions required for 
